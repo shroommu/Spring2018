3a109bf0e16f213b909eb6f4fd483b62b01df9f3
Alexa Kruckenberg <alexa.kruckenberg@gmail.com>
2018-03-03 20:12:01 -0700

    created unity proj for char dev, created scene for anthony's assignment

---
+++ Character Dev Sandbox/Assets/Scripts/Elf.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elf : MonoBehaviour {
+
+	public SO_Elf so_elf;
+	public GameObject geometry;
+	private SkinnedMeshRenderer meshRenderer;
+
+	void Start()
+	{
+		meshRenderer = geometry.GetComponent<SkinnedMeshRenderer>();
+	}
+
+	public void OnCharacterSelect()
+	{
+		if(so_elf != null)
+		{
+			PrintName();
+			ChangeColor();
+			PrintHealth();
+		}
+	}
+
+	public void PrintName()
+	{
+		Debug.Log("Name: " + so_elf.charName);
+	}
+
+	public void ChangeColor()
+	{
+		Material[] intMaterials;
+		intMaterials = new Material[meshRenderer.materials.Length];
+
+		for(int i=0; i<intMaterials.Length;i++)
+		{
+        	intMaterials[i] = so_elf.material;
+		}
+
+     	meshRenderer.materials = intMaterials;
+	}
+
+	public void PrintHealth()
+	{
+		Debug.Log("Health: " + so_elf.health);
+	}
+
+
+}
---
+++ Character Dev Sandbox/Assets/Scripts/OnExitGameEventBehavior.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RoboRyanTron.Unite2017.Events;
+
+public class OnExitGameEventBehavior : StateMachineBehaviour {
+
+	public GameEvent gameEvent;
+
+	void OnStateExit (){
+		gameEvent.Raise();
+	}
+}
