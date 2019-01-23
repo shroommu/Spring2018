03dd4a8b4bcd7548884ee18a5471f17f990c9409
Alexa Kruckenberg <alexa.kruckenberg@gmail.com>
2019-01-22 17:15:26 -0700

    added level design unity project removed large file

---
+++ Unity/Level Design/Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Made By Anthony Romrell
+public class CharacterMove : MonoBehaviour 
+{
+	public float speed = 6.0F;
+	public float sprintSpeed = 12.0F;
+	public float jumpSpeed = 8.0F;
+	public float gravity = 20.0F;
+	private Vector3 moveDirection;
+	private Vector3 rotateDirection;
+
+	private CharacterController controller;
+
+	private void Start()
+	{
+		controller = GetComponent<CharacterController>();
+	}
+
+	void Update() {
+		if (controller.isGrounded)
+		{
+			//rotateDirection.y = Input.GetAxis("Horizontal");
+			transform.Rotate(rotateDirection);
+			moveDirection.Set(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+
+			if(Input.GetKey(KeyCode.LeftShift))
+			{
+				moveDirection *= sprintSpeed;
+			}
+			else
+			{
+				moveDirection *= speed;
+			}
+
+			if (Input.GetButton("Jump"))
+				moveDirection.y = jumpSpeed;
+            
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+	}	
+}
---
+++ Unity/Level Design/Assets/Scripts/LockCursor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockCursor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+}
